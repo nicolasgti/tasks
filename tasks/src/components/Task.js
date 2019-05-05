cf51405ffd32f353997fb47ba538a2803f6059dc
Nicolas <nicolasgti@hotmail.com>
2019-05-04 22:00:24 -0400

    correção do task
    botão funcioandndo

--- tasks/src/components/Task.js
+++ tasks/src/components/Task.js
@@ -9,13 +9,13 @@ export default props => {
     let check = null
     if (props.doneAt !== null) {
         check = (
-            <View style={StyleSheet.done}>
+            <View style={styles.done}>
                 <Icon name='check' size={20}
                     color={commonStyles.colors.secondary}/>
             </View>
         )
     }else {
-        check = <View style={StyleSheet.pending}/>
+        check = <View style={styles.pending}/>
     }
 
     const descStyle = props.doneAt !== null ?
