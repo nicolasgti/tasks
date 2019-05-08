de5dd8d8a6206cc24e7b677d612ca9479ef30f71
Nicolas <nicolasgti@hotmail.com>
2019-05-08 08:44:14 -0400

    Componente tarefa
    Marcar e desmarcar uma tarefa

--- tasks/src/screens/Agenda.js
+++ tasks/src/screens/Agenda.js
@@ -33,6 +33,17 @@ export default class agenda extends Component {
         ]
     }
 
+    toggleTask = id => {
+        const tasks = this.state.tasks.map(task => {
+            if (task.id === id){
+                task = {...task}
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+            return task
+        })
+        this.setState({tasks})
+    }
+
     render(){
         return (
             <View style={styles.container}>
@@ -47,7 +58,8 @@ export default class agenda extends Component {
                 <View style={styles.taskContainer}>
                     <FlatList data={this.state.tasks}
                         keyExtractor={item => `${item.id}`}
-                        renderItem={({item}) => <Task {...item} />} />
+                        renderItem={({item}) => 
+                            <Task {...item} toggleTask={this.toggleTask} />} />
                 </View>
             </View>
         )
